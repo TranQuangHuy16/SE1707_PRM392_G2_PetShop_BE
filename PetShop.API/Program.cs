5920e0b85b980e5cf17ea78acafc8bfbc6963fc5
tomhuyquang@gmail.com <tomhuyquang@gmail.com>
2025-11-01 21:17:16 +0700

    Prepare for Render deploy

--- PetShop.API/Program.cs
+++ PetShop.API/Program.cs
@@ -12,6 +12,10 @@ using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+
 // ============ JWT AUTHENTICATION ============
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer("Bearer", options =>
